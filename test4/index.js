b16ba69f42ffdeadc3d233fe0c606d37e79cf597
tuohai <tuohaislashboy@163.com>
2022-03-21 21:59:10 +0800

    feat: temp

---
+++ test4/index.js
@@ -0,0 +1,5 @@
+var objt = {
+    label: '',
+    option: ''
+};
+console.log(objt);
---
+++ test4/index.ts
@@ -0,0 +1,12 @@
+type option = {
+  label: string
+  option: string
+  disable?: string
+}
+
+const objt:option = {
+  label: '',
+  option: '',
+}
+
+console.log(objt)
